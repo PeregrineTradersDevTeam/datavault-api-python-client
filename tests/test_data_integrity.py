d89264de636f2999d87f03ff257032fdc7284f7f
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-12-04 11:33:40 +0100

    Added tests for the data_integrity_test function.

--- tests/test_data_integrity.py
+++ tests/test_data_integrity.py
@@ -54,3 +54,58 @@ class TestCheckSize:
         # Verify
         assert calculated_size == true_file_size
         # Cleanup - none
+
+
+class TestDataIntegrityTest:
+    def test_integrity_testing_fail_scenario(self):
+        # Setup
+        file_download_details = DownloadDetails(
+            file_name='WATCHLIST_367_20200721.txt.bz2',
+            download_url=(
+                'https://api.icedatavault.icedataservices.com/v2/data/2020/07/21/S367/'
+                'WATCHLIST/20200721-S367_WATCHLIST_username_0_0'
+            ),
+            file_path=pathlib.Path(__file__).resolve().parent.joinpath(
+                'Data', '2020', '07', '21', 'S367', 'WATCHLIST', 'WATCHLIST_367_20200721.txt.bz2',
+            ),
+            size=82451354,
+            md5sum='62df718ef5eb5f9f1ea3f6ea1f826c30',
+            is_partitioned=True
+        )
+        file_directory = file_download_details.file_path.parent
+        file_directory.mkdir(parents=True, exist_ok=True)
+        with file_download_details.file_path.open('wb') as outfile:
+            random_file_content = os.urandom(5000)
+            outfile.write(random_file_content)
+        # Exercise
+        outcome = data_integrity.data_integrity_test(file_download_details)
+        # Verify
+        expected_outcome = False
+        assert outcome == expected_outcome
+        # Cleanup - none
+        # First, remove all the created files
+        for file in list(file_directory.glob('**/*.bz2')):
+            file.unlink()
+        # Then remove all the created folders iteratively:
+        directory_root = pathlib.Path(__file__).resolve().parent / 'Data'
+        for directory in list(directory_root.glob('**/'))[::-1]:
+            directory.rmdir()
+
+    def test_integrity_testing_pass_scenario(self):
+        # Setup
+        file_download_details = DownloadDetails(
+            file_name='CROSSREF_367_20200721.txt.bz2',
+            download_url='https://api.icedatavault.icedataservices.com/v2/data/2020/07/21/'
+                         'S367/CROSS/20200721-S367_CROSS_ALL_0_0',
+            file_path=pathlib.Path(__file__).resolve().parent.joinpath(
+                'static_data', 'CROSSREF_367_20200721_simulated.txt.bz2'
+            ),
+            size=924,
+            md5sum='d742203115d9637199386ac8d71cc4cd',
+            is_partitioned=False)
+        # Exercise
+        outcome = data_integrity.data_integrity_test(file_download_details)
+        # Verify
+        expected_outcome = True
+        assert outcome == expected_outcome
+        # Cleanup - none
