5bcd055eb504092c6a6428a9c684430b4221bc7b
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-12-07 18:25:23 +0100

    Adds synchronous download functions.

--- src/datavault_api_client/downloaders.py
+++ src/datavault_api_client/downloaders.py
@@ -4,11 +4,16 @@ import concurrent.futures
 from itertools import repeat
 import pathlib
 import threading
+from typing import List, Tuple
 
+import click
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 
+from datavault_api_client.data_structures import DownloadDetails
+from datavault_api_client.connectivity import create_session
+from datavault_api_client.data_integrity import get_list_of_failed_downloads
 
 thread_local = threading.local()
 
@@ -40,3 +45,51 @@ def thread_get_session() -> requests.Session:
             ),
         )
     return thread_local.session
+
+
+def download_file(download_info: DownloadDetails, credentials: tuple, session: requests.Session):
+    download_url = download_info.download_url
+    file_path = download_info.file_path
+    pathlib.Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+    # TODO: add logging to the function instead of using click.echo().
+    click.echo(f"# Downloading {download_url} ...")
+    with session.get(download_url, auth=credentials, stream=True) as response:
+        # TODO: think about inserting a try-except block instead of using the status code check.
+        if response.status_code == 200:
+            with file_path.open("wb") as output:
+                for chunk in response.iter_content(chunk_size = 3 * 1024 * 1024):
+                    output.write(chunk)
+    # TODO: add logging to the function instead of using click.echo()
+    click.echo(f"+ Download completed: {pathlib.Path(file_path).as_posix()}")
+
+
+def download_files_synchronously(
+    download_manifest: List[DownloadDetails],
+    credentials: Tuple[str, str],
+    max_number_of_download_attempts: int = 5,
+    current_attempt=None,
+) -> None:
+    if current_attempt is None:
+        current_attempt = 1
+
+    session = create_session()
+    for file in download_manifest:
+        download_file(file, credentials, session)
+
+    failed_downloads = get_list_of_failed_downloads(download_manifest)
+
+    if len(failed_downloads) > 0:
+        if current_attempt <= max_number_of_download_attempts:
+            download_files_synchronously(
+                failed_downloads,
+                credentials,
+                max_number_of_download_attempts=max_number_of_download_attempts,
+                current_attempt=current_attempt+1,
+            )
+        else:
+            for failed_download in failed_downloads:
+                # TODO: add logging to the function instead of using click.echo()
+                click.echo(f"- Failed to download: {failed_download.file_name}")
+    else:
+        # TODO: add logging to the function instead of using click.echo()
+        click.echo("All files successfully downloaded.")
