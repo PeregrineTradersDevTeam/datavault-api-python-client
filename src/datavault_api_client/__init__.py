9dec46a2025eaa21a4ad79141f9983c99e016818
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-12-21 19:24:29 +0100

    Removes summary_utils from the import statement and the __all__ variable.

--- src/datavault_api_client/__init__.py
+++ src/datavault_api_client/__init__.py
@@ -1,13 +1,19 @@
 # This file is licensed under the terms of the MIT License.
-# See the LICENSE file in the root of this repository 
+# See the LICENSE file in the root of this repository
 # for complete details.
 
-"""
-Datavault API Client Library for Python
-"""
+"""Datavault API Client Library for Python."""
 
 
-from datavault_api_client import connectivity, crawler, data_integrity, data_structures, donwloaders, post_download_processing, pre_download_processing, summary_utils
+from datavault_api_client import (
+    connectivity,
+    crawler,
+    data_integrity,
+    data_structures,
+    downloaders,
+    post_download_processing,
+    pre_download_processing,
+)
 
 
 __version__ = "0.1.0"
@@ -19,7 +25,7 @@ __author__ = "Jacopo Abbate"
 __email__ = "jacopo.abbate@peregrinetraders.com"
 
 __license__ = "MIT License"
-__coyright__ = "copyright (c) 2020 Peregrine Traders B.V."
+__copyright__ = "copyright (c) 2020 Peregrine Traders B.V."
 
 
 __all__ = [
@@ -28,7 +34,6 @@ __all__ = [
     "data_integrity",
     "data_structures",
     "downloaders",
-    "post_download_processing", 
+    "post_download_processing",
     "pre_download_processing",
-    "summary_utils",
 ]
