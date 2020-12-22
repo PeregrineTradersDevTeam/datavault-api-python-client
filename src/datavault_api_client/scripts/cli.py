31a892809c1aba247427017469bc99e45bfb6ca4
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-12-22 14:23:47 +0100

    Adds the import statements.

--- src/datavault_api_client/scripts/cli.py
+++ src/datavault_api_client/scripts/cli.py
@@ -1,4 +1,12 @@
-"""
-Module containing the command line app.
-"""
-import click 
+"""Module containing the command line app."""
+import click
+
+from datavault_api_client.crawler import datavault_crawler
+from datavault_api_client.downloaders import (
+    download_files_concurrently,
+    download_files_synchronously,
+)
+from datavault_api_client.pre_download_processing import (
+    pre_concurrent_download_processor,
+    pre_synchronous_download_processor,
+)
