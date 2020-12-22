af598395000da711c3d13d21ed98080e44426087
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-12-22 15:50:55 +0100

    Implements the downloading functions.

--- src/datavault_api_client/downloaders.py
+++ src/datavault_api_client/downloaders.py
@@ -1,6 +1,7 @@
 """Implements the downloading functions."""
 
 import concurrent.futures
+import itertools
 from itertools import repeat
 import pathlib
 import threading
@@ -11,9 +12,11 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 
-from datavault_api_client.data_structures import DownloadDetails
 from datavault_api_client.connectivity import create_session
 from datavault_api_client.data_integrity import get_list_of_failed_downloads
+from datavault_api_client.data_structures import ConcurrentDownloadManifest, DownloadDetails
+from datavault_api_client.post_download_processing import post_concurrent_download_processing
+
 
 thread_local = threading.local()
 
@@ -57,7 +60,7 @@ def download_file(download_info: DownloadDetails, credentials: tuple, session: r
         # TODO: think about inserting a try-except block instead of using the status code check.
         if response.status_code == 200:
             with file_path.open("wb") as output:
-                for chunk in response.iter_content(chunk_size = 3 * 1024 * 1024):
+                for chunk in response.iter_content(chunk_size=3 * 1024 * 1024):
                     output.write(chunk)
     # TODO: add logging to the function instead of using click.echo()
     click.echo(f"+ Download completed: {pathlib.Path(file_path).as_posix()}")
@@ -93,3 +96,47 @@ def download_files_synchronously(
     else:
         # TODO: add logging to the function instead of using click.echo()
         click.echo("All files successfully downloaded.")
+
+
+def thread_safe_download(download_info: DownloadDetails, credentials: Tuple[str, str]):
+    session = thread_get_session()
+    download_file(download_info, credentials, session)
+
+
+def download_files_concurrently(
+    concurrent_download_manifest: ConcurrentDownloadManifest,
+    credentials: Tuple[str, str],
+    max_number_of_workers: int = None,
+    max_number_of_download_attempts: int = 5,
+    current_attempt: int = None,
+) -> None:
+    if current_attempt is None:
+        current_attempt = 1
+
+    files_to_download = list(itertools.chain(
+        concurrent_download_manifest.whole_files_to_download,
+        concurrent_download_manifest.partitions_to_download,
+    ))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_number_of_workers) as executor:
+        executor.map(
+            thread_safe_download,
+            files_to_download,
+            repeat(credentials),
+        )
+
+    failed_downloads = post_concurrent_download_processing(concurrent_download_manifest)
+
+    if len(failed_downloads.files_reference_data) > 0:
+        click.echo(f'Failed to download {len(failed_downloads.files_reference_data)} file(s).')
+        if current_attempt <= max_number_of_download_attempts:
+            download_files_concurrently(
+                failed_downloads,
+                credentials,
+                max_number_of_workers=max_number_of_workers,
+                max_number_of_download_attempts=max_number_of_download_attempts,
+                current_attempt=current_attempt + 1)
+        else:
+            for failed_download in failed_downloads.files_reference_data:
+                click.echo(f'- Failed to download: {failed_download.file_name}')
+    else:
+        click.echo('All files successfully downloaded.')
